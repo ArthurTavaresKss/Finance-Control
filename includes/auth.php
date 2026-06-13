50243e26a0da4bccaf1c3418d519ce3fa2a3c36b
Arthur Tavares <arthurtavareskss@gmail.com>
2026-06-13 13:59:39 -0300

    Correção do caminho de Auth
    Correção do caminho do redirect da Auth para /public/login.php

--- includes/auth.php
+++ includes/auth.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../includes/functions.php';
 session_start();
 if (!isset($_SESSION['idUsuario'])) {
-    redirect("/Finance-Control/login.php");
+    redirect("/Finance-Control/public/login.php");
     exit;
 }
 ?>
