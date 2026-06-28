f9ed9b7066d70a31391971f6379d9b6f82c963ab
Arthur Tavares <arthurtavareskss@gmail.com>
2026-06-28 19:46:57 -0300

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../includes/functions.php';
 session_start();
 
-define('TEMPO_LIMITE_INATIVIDADE', 60 * 60); // 1 hora
+define('TEMPO_LIMITE_INATIVIDADE', 10 * 1); // 1 hora
  
 if (!isset($_SESSION['idUsuario'])) {
     redirect("/Finance-Control/public/login.php");
@@ -15,7 +15,7 @@ if (isset($_SESSION['ultimo_acesso'])) {
     if ($tempoInativo > TEMPO_LIMITE_INATIVIDADE) {
         session_unset();
         session_destroy();
-        redirect("/Finance-Control/public/login.php?status=sessao_expirada");
+        redirect("login?status=sessao_expirada");
         exit;
     }
 }
